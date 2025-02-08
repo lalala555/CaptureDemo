e6399fe498e55b91596e938b48b1667831d29bbb
liuzizhuo <jiangshaozhong@srodcn.com>
2025-02-08 16:27:46 +0800

    tt

--- CaptureDemo/main.cpp
+++ CaptureDemo/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
       QSharedMemory shared_memory;
     shared_memory.setKey(QString("PipeClimber"));
     if(shared_memory.attach())
-        return 0;
+        return -1;
     if(shared_memory.create(1))
     {
         MainWindow mainWindow;
